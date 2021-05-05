497895b7d303ab421acf5fa6fe62dbb430a1d9b5
rsilva42 <rsilva@student.42.us.org>
2021-05-05 00:04:17 -0400

    add word bank edit functions

--- hangman.py
+++ hangman.py
@@ -29,19 +29,22 @@ def main_menu(word_bank={}):
 	sets up a user interface, giving the options:
 		"play": to play the game
 		"add": to add a category or a word to the word bank
+		"remove": to remove a category or a word from the word bank
 		"exit": to exit the input loop
 
 	if no word bank is supplied an empty word bank will be initialized'''
 
 	#infinite loop until user inputs 'exit'
 	while True:
-		user_input = input("What would you like to do?\nOptions are: 'play', 'add', and 'exit'\n").lower()
+		user_input = input("What would you like to do?\nOptions are: 'play', 'add', 'remove' and 'exit'\n").lower()
 
 		try:
 			if user_input == "play":
 				select_category(word_bank)
 			elif user_input == "add":
 				bank_add(word_bank)
+			elif user_input == "remove":
+				bank_remove(word_bank)
 			elif user_input == "exit":
 				break
 			else:
@@ -57,23 +60,28 @@ def select_category(word_bank):
 	when selecting a category the user is also given the options:
 		"random": to let the program choose a random category
 		"list": to list the categories in the current word bank'''
+	user_input = "yes"
 
-	while True:
+	while user_input == "yes":
 		user_input = input("Insert a category.\nYou may also try 'random' for a random category and 'list' for a list of the categories in the word bank\n").lower()
 
-		#respond to user input accordingly
 		#if input is list then print the categories and loop back
 		if user_input == "list":
 			print("The current bank contains:")
 			for category in list(word_bank.keys()):
 				print(category)
 			continue
-		#else fill category variable for the game to use
+
+		#if input is random then fill category variable with a random category
 		elif user_input == "random":
 			category = random.choice(list(word_bank.keys()))
+
+		#if input is not a category, print an error and ask for input again
 		elif user_input not in list(word_bank.keys()):
 			print("Category not found, try again...")
 			continue
+
+		#fill category with input
 		else:
 			category = user_input
 		print("The category is {}".format(category))
@@ -84,9 +92,7 @@ def select_category(word_bank):
 		hangman_guess(word)
 
 		#allow the player to try again or exit
-		user_input = input("Try again? yes/no").lower
-		if user_input == "yes":
-			break
+		user_input = input("Try again? yes/no\n").lower()
 
 def hangman_guess(word, lives=10):
 	'''usage: hangman_guess(lives=10, word)
@@ -94,7 +100,7 @@ def hangman_guess(word, lives=10):
 	will allow the user 'lives' amount of wrong guesses to complete the word one letter at a time.
 	upon completion print a congratulations and exit'''
 
-	#create a set of blanks for the letters guessed right
+	#create a set of blanks for the letters guessed right and an empty string of letters already guessed
 	blanks = [ "_" for i in range(len(word)) ]
 	letters_used = ""
 
@@ -104,9 +110,11 @@ def hangman_guess(word, lives=10):
 		#check if only one letter was input
 		if len(guess) != 1:
 			print("Only one letter at a time please!")
+
 		#check if the letter has been guessed already
 		elif guess in letters_used:
 				print("You already guessed that character!")
+
 		else:
 			if guess in word:
 
@@ -121,7 +129,7 @@ def hangman_guess(word, lives=10):
 				lives -= 1
 				print("wrong, lives left: {}".format(lives))
 
-			letters_used += (guess)
+			letters_used += guess
 
 			#if no blanks are left the game is won
 			if not '_' in blanks:
@@ -129,25 +137,89 @@ def hangman_guess(word, lives=10):
 				return
 
 	#if the loop is broken, lives hit 0
-	print("you ran out of lives!")
+	print("you ran out of lives!\nGAME OVER")
 
 def display_blanks(blanks):
+	'''usage: display_blanks(blanks)
+
+	where blanks is a list of characters to be printed in hangman format
+	ex: "_ i _ _ _"'''
+
 	for char in blanks:
 		print(char, end=' ')
+	#empty print for the new line at the end
 	print('')
 
 def bank_add(word_bank):
-	print("add invoked")
 	user_input = input("What would you like to add?\nOptions are: 'category' and 'word'\n").lower()
-	print(user_input)
 
-#TODO define play and its options to select a category or to use a random category
-#also handle the error for if the word bank is empty
-#define add and its options to add a category or a word to a category
-#maybe reformat the user loop to work within one try except to catch all errors at once
+	if user_input == "category":
+		add_category(word_bank)
+	elif user_input == "word":
+		add_word(word_bank)
+
+def add_category(word_bank):
+	category_to_add = input("What category would you like to add?\n").lower()
+
+	user_input = input("Is {} good? yes/no (this will overwrite existing categories)\n".format(category_to_add)).lower()
+
+	if user_input == "yes":
+		word_bank[category_to_add] = []
+		print("{} category added".format(category_to_add))
 
 
+def add_word(word_bank):
+	category = input("What category would you like to add the word to?\n").lower()
 
+	if (category in word_bank.keys()):
+		word_to_add = input("What is the word?\n").lower()
+
+		user_input = input("Is {} good? yes/no\n".format(word_to_add)).lower()
+
+		if user_input == "yes":
+			word_bank[category].append(word_to_add)
+			print("Word {} added".format(word_to_add))
+	else:
+		print("category not found, please try again...")
+
+def bank_remove(word_bank):
+	user_input = input("What would you like to remove?\nOptions are: 'category' and 'word'\n").lower()
+
+	if user_input == "category":
+		remove_category(word_bank)
+	elif user_input == "word":
+		remove_word(word_bank)
+
+def remove_category(word_bank):
+	category_to_remove = input("What category would you like to remove?\n").lower()
+
+	if category_to_remove in word_bank.keys():
+		user_input = input("Are you sure you want to remove the {} category? yes/no (this cannot be undone)\n".format(category_to_remove)).lower()
+
+		if user_input == "yes":
+			word_bank.pop(category_to_remove)
+			print("{} category removed".format(category_to_remove))
+	else:
+		print("{} category does not exist".format(category_to_remove))
+
+def remove_word(word_bank):
+	category = input("In which category is the word you would like to remove?\n").lower()
+
+	if (category in word_bank.keys()):
+		word_to_remove = input("What is the word?\n").lower()
+
+		if word_to_remove in word_bank[category]:
+			user_input = input("Are you sure you want to remove {}? yes/no (this cannot be undone)\n".format(word_to_remove)).lower()
+
+			if user_input == "yes":
+				word_bank[category].remove(word_to_remove)
+				print("Word {} removed".format(word_to_remove))
+		else:
+			print("{} word not found".format(word_to_remove))
+	else:
+		print("category not found, please try again...")
+
+#TODO documentation for add and remove functions
 
 if __name__ == '__main__':
 	main()
